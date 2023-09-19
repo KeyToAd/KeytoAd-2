bd9ccfd2351f83828751731ff3525d1cfe24ebd0
KBungs <64015011@kmitl.ac.th>
2023-09-19 14:47:54 +0700

    test

--- Palka Code/seleniumm.py
+++ Palka Code/seleniumm.py
@@ -13,6 +13,7 @@ chrome_options = Options()
 # chrome_options.add_argument('--disable-dev-shm-usage')
 
 def scrape_page(i):
+    i+=17
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(f"https://www.watsons.co.th/th/search?text=%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%87%E0%B8%AA%E0%B8%B3%E0%B8%AD%E0%B8%B2%E0%B8%87&useDefaultSearch=false&sortCode=bestSeller&pageSize=64&currentPage={i}")
     time.sleep(5)
@@ -38,13 +39,21 @@ def scrape_page(i):
         # Keep all link in page
         div_name = soup.find('div', class_='product-name')
         div_detail = soup.find('div', class_='product-details')
-        lst_name.append(div_name.text)
-        lst_detail.append(div_detail.text)
+        try:
+            lst_name.append(div_name.text)
+        except:
+            lst_name.append("")
+
+        try:
+            lst_detail.append(div_detail.text)
+        except:
+            lst_detail.append("")
+        
     driver.quit()
     return lst_name, lst_detail
 
 if __name__ == "__main__":
-    num_pages = 93
+    num_pages = 5
     num_processes = multiprocessing.cpu_count()  # You can adjust this as needed
 
     pool = multiprocessing.Pool(processes=num_processes)
@@ -60,4 +69,5 @@ if __name__ == "__main__":
         all_details.extend(details)
 
     df = pd.DataFrame(list(zip(all_names, all_details)), columns=['Name', 'Detail'])
-    df.to_csv("Watson64.csv", index=False)
+    print(all_names)
+    df.to_csv("Watson104.csv", index=False)
